a2bbd69f6bbf7724671522bc257ed68c2f0b55e5
Romeo <romeojeremiah@gmail.com>
2019-09-25 20:22:47 -0500

    change variable from var to let

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var button = document.getElementById('btn');
+let button = document.getElementById('btn');
 
 button.addEventListener('click', function(){
     let word = document.getElementById('str').value;
@@ -6,4 +6,4 @@ button.addEventListener('click', function(){
     let outputDiv = document.getElementById('output');
 
     outputDiv.innerHTML = `<h1>${count}</h1>`
-})
+});
